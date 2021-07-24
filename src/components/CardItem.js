eae9d94157bc5ba79ee4248127cc67ad26d9a98a
ChaseTheCoder <chasefsheaff@gmail.com>
2021-07-23 23:07:03 -0500

    fix: package.json homepage

---
+++ src/components/CardItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function CardItem() {
+  return(
+    <>
+    <li className="cards_item">
+      <Link className="cards_item_link">
+        <figure className="cards_item_pic-wrap">
+          <img src="/" alt="Travel Image" className="cards_item_img"/>
+        </figure>
+      </Link>
+    </li>
+    </>
+  )
+}
